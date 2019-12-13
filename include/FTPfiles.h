84ef2de98524250ff429689fdc938a81c1a49beb
boikov3 <55747325+boikov3@users.noreply.github.com>
2019-12-13 21:24:26 +0300

    1

--- include/FTPfiles.h
+++ include/FTPfiles.h
@@ -18,7 +18,7 @@ struct file{
     string type;
 };
 static string mypath="D:\\LABB01\\laba4-master";
-static int pz=mypath.length()-4;
+static int pz=mypath.length()+1;
 string getLatestDate(const string &date1, const string &date2);
 void printFinFile(const path &p, map<string, pair <unsigned int, string>>& accounts);
 void printAccountsInfo(const path &path_to_dir, map<string, pair <unsigned int, string>>& accounts);
